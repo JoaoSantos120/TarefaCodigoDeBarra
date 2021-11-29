a3f6b920cac15316514cd30d66497298839ea4e4
JoaoSantos120 <joaohenriqstos12@gmail.com>
2021-11-30 03:45:20 +0900

    Indo12

--- www/components/calc.js
+++ www/components/calc.js
@@ -2,7 +2,31 @@
 
 window.onload = function(){
   document.querySelector('#exibir').addEventListener('click', function(){
-    document.querySelector('#texto').innerHTML = "<h2>Informações</h2>";
-    document.querySelector('#info').textContent = "Motorola";
+
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        if(result.text = '1234'){
+          document.querySelector('#texto').innerHTML = "<h2>Informações</h2>";
+          document.querySelector('#info').textContent = "Motorola";
+        }
+
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
   });
 }
