31985f3bfd9a54f6f6dc8a70553b04a70fd62a3c
JoaoSantos120 <joaohenriqstos12@gmail.com>
2021-12-01 04:58:36 +0900

    Um saco :/

--- www/components/calc.js
+++ www/components/calc.js
@@ -5,9 +5,21 @@ window.onload = function(){
 
     cordova.plugins.barcodeScanner.scan(
       function (result) {
-        if(result.text = '1234'){
+        if(result.text = '280720550'){
           document.querySelector('#texto').innerHTML = "<h2>Informações</h2>";
-          document.querySelector('#info').textContent = "Motorola";
+          document.querySelector('#info').textContent = "Nome do Produto: iPad Mini Wi-Fi 64Gb - Cinza Espacial";
+          document.querySelector('#img').src = 'lib/img/imagem1.jpg';
+           document.querySelector('#info').textContent = "Valor: R$ 3.499,00 
+           Descrição:
+          Processador A12 Bionic com arquitetura de 64 bits Neural Engine Coprocessador M12 integrado
+          Sistema operacional iOS Tamanho da tela 7.9" Tecnologia Wi-Fi
+          Resolução da câmera - Câmera traseira: 8MP; - Câmera frontal: 7MP 
+          GPS Sim
+          Grava vídeos Sim
+          Câmera traseira Sim
+          Câmera frontal Sim
+          Memória Flash 64 GB
+          Funciona como celular: Não";
         }
 
       },
